042c4fa53d30b2563c79a33f1194822c8d500b06
anqiushi767 <anqiushi767@gmail.com>
2026-08-03 11:56:08 +0800

    feat: 老旧浏览器检测弹窗 + 移动端性能降级恢复

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import BootScreen from './components/BootScreen.vue'
 import CustomCursor from './components/CustomCursor.vue'
@@ -10,6 +10,16 @@ import PageShutter from './components/PageShutter.vue'
 import ScrollProgress from './components/ScrollProgress.vue'
 
 const booted = ref(false)
+const showBrowserTip = ref(false)
+
+onMounted(() => {
+  const ua = navigator.userAgent
+  // 国产套壳浏览器
+  const domestic = /QQBrowser|MQQBrowser|360SE|360EE|Sogou|SE\s|MetaSr|UCBrowser|UCWEB|LieBao|baidubrowser/i.test(ua)
+  // 旧版 Chrome (< 100) 或 Microsoft Edge 旧内核
+  const oldChrome = /Chrome\/(\d+)/.test(ua) && parseInt(RegExp.$1) < 100
+  if (domestic || oldChrome) showBrowserTip.value = true
+})
 
 // 页面切换：先播放快门帘，再执行路由跳转
 const router = useRouter()
@@ -36,4 +46,11 @@ router.beforeEach((to, from, next) => {
     </router-view>
   </main>
   <FooterBar />
+  <Transition name="toast">
+    <div v-if="showBrowserTip" class="browser-tip">
+      <span>⚠️ 您的浏览器版本过低，可能无法正常显示本站效果。</span>
+      <a href="https://www.google.cn/chrome/" target="_blank">下载最新版 Chrome →</a>
+      <button @click="showBrowserTip = false">✕</button>
+    </div>
+  </Transition>
 </template>
